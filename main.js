5f22015d8982f6f6d48c49352464bbb177c5ccb6
Axel Scheele <axel@Axels-MacBook-Pro.local>
2017-05-12 11:44:04 +0200

    set ids

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const builder = require('botbuilder')
 
 module.exports = function(app) {
 
-    var connector = new builder.ChatConnector({})
+    var connector = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    })
 
     app.use('/', connector.listen())
 
